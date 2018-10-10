3fb40fa58e2c7d2839b95afb76dcba6e9261f078
Rekola <rekolama@gmail.com>
2018-10-10 10:05:09 +0300

    Lisätty controller-toimintoja (route).

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -9,8 +9,18 @@ using WebDemo.Models;
 namespace WebDemo.Controllers
 {
     [Route("api/[controller]")]
-    public class CustomerController : Controller
+    public class CustomerController : Controller // pienillä ja isoilla kirjaimilla ei tässä merkitystä
     {
+
+            [Route("moikka")]   //https://localhost:5001/api/customer/moikka
+                        public string GetMoi() {
+                    return "Moikka!";
+            }
+
+            [Route("luvut")]  //https://localhost:5001/api/customer/luvut
+                        public int[] GetLuvut() {
+                    return new int[] { 1,2,3,4,5 };
+            }
 
         public List<Customers> GetCustomers()
         {
